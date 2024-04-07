aa4d9765ad20424aa69143c69dc3bef502ffe215
Simon Revill <simon.revill@vertumotors.com>
2024-04-07 20:16:44 +0100

    refactor adds new describe block for cube

--- src/Cube/Cube.test.ts
+++ src/Cube/Cube.test.ts
@@ -1,18 +1,20 @@
 import Cube from './Cube';
 
-describe('cubic centimetre volume tests', () => {
-  it('creates a cube with length of 10', () => {
-    const cube = new Cube(10, 10, 10);
-    expect(cube.length).toBe(10);
-  });
+describe('cube tests', () => {
+  describe('cubic centimetre volume tests', () => {
+    it('creates a cube with length of 10', () => {
+      const cube = new Cube(10, 10, 10);
+      expect(cube.length).toBe(10);
+    });
 
-  it('creates a cube with width of 10', () => {
-    const cube = new Cube(10, 10, 10);
-    expect(cube.width).toBe(10);
-  });
+    it('creates a cube with width of 10', () => {
+      const cube = new Cube(10, 10, 10);
+      expect(cube.width).toBe(10);
+    });
 
-  it('creates a cube with height of 10', () => {
-    const cube = new Cube(10, 10, 10);
-    expect(cube.height).toBe(10);
+    it('creates a cube with height of 10', () => {
+      const cube = new Cube(10, 10, 10);
+      expect(cube.height).toBe(10);
+    });
   });
 });
