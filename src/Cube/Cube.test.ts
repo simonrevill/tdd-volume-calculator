93a51cb3f87bd6255aed881ac73461ffaa7ad93d
Simon Revill <simon.revill@vertumotors.com>
2024-04-07 20:22:47 +0100

    refactor tests - add space for arrange and assert

--- src/Cube/Cube.test.ts
+++ src/Cube/Cube.test.ts
@@ -4,21 +4,25 @@ describe('cube tests', () => {
   describe('cubic centimetre volume tests', () => {
     it('creates a cube with length of 10', () => {
       const cube = new Cube(10, 10, 10);
+
       expect(cube.length).toBe(10);
     });
 
     it('creates a cube with width of 10', () => {
       const cube = new Cube(10, 10, 10);
+
       expect(cube.width).toBe(10);
     });
 
     it('creates a cube with height of 10', () => {
       const cube = new Cube(10, 10, 10);
+
       expect(cube.height).toBe(10);
     });
 
     it('creates a cube with the volume of 1000', () => {
       const cube = new Cube(10, 10, 10);
+
       expect(cube.getAreaInCubicCentimetres()).toBe(1000);
     });
   });
