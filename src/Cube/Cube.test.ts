6f05e36b01de993b55c7c4e7433143e0303614ae
Simon Revill <simon.revill@vertumotors.com>
2024-04-07 20:14:56 +0100

    adds test for cube with width

--- src/Cube/Cube.test.ts
+++ src/Cube/Cube.test.ts
@@ -1,8 +1,13 @@
 import Cube from './Cube';
 
 describe('cubic centimetre volume tests', () => {
-  it('creates a cube with length', () => {
+  it('creates a cube with length of 10', () => {
     const cube = new Cube(10, 10, 10);
     expect(cube.length).toBe(10);
+  });
+
+  it('creates a cube with width of 10', () => {
+    const cube = new Cube(10, 10, 10);
+    expect(cube.width).toBe(10);
   });
 });
