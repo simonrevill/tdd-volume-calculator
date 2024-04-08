a6913621c47f7c3969b13104545794023aa13b82
Simon Revill <simon.revill@vertumotors.com>
2024-04-08 14:40:05 +0100

    adds litre calculation for the pyramid sold. refactor getRequiredLitresOfWater() method

--- src/VolumeCalculator/VolumeCalculator.test.ts
+++ src/VolumeCalculator/VolumeCalculator.test.ts
@@ -1,4 +1,5 @@
 import Cube from '../Cube';
+import Pyramid from '../Pyramid';
 import VolumeCalculator from './VolumeCalculator';
 
 describe('volume calculator tests', () => {
@@ -8,6 +9,15 @@ describe('volume calculator tests', () => {
       const volumeCalculator = new VolumeCalculator();
 
       expect(volumeCalculator.getRequiredLitresOfWater(cube)).toBe(1);
+    });
+  });
+
+  describe('pyramid litre tests', () => {
+    it('gets the volume of a pyramid in litres which is 1', () => {
+      const pyramid = new Pyramid(10, 10, 10);
+      const volumeCalculator = new VolumeCalculator();
+
+      expect(volumeCalculator.getRequiredLitresOfWater(pyramid)).toBe(0.33);
     });
   });
 });
--- src/VolumeCalculator/VolumeCalculator.ts
+++ src/VolumeCalculator/VolumeCalculator.ts
@@ -2,6 +2,10 @@ import { Solid } from '../types';
 
 export default class VolumeCalculator {
   getRequiredLitresOfWater(solid: Solid): number {
-    return solid.getAreaInCubicCentimetres() / 1000;
+    const litres = solid.getAreaInCubicCentimetres() / 1000;
+
+    const result = Number(litres.toFixed(2));
+
+    return result;
   }
 }
